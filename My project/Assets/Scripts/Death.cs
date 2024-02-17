ca9d192251dffa3fbb6ca5d1740a9f2b85f15d9f
Ivan <opciedjycyt@gmail.com>
2024-02-17 14:44:14 +0800

    feat: 新增Game over 腳本以及 TimeManager

--- My project/Assets/Scripts/Death.cs
+++ My project/Assets/Scripts/Death.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Death : MonoBehaviour
 {
+    //Unity Inspector
+
+    [SerializeField] private UnityEvent died_;
+
     public void CheckDeath(int health)
     {
         if (health <= 0)
@@ -15,5 +20,6 @@ public class Death : MonoBehaviour
     public void Die()
     {
         gameObject.SetActive(false);
+        died_.Invoke();
     }
 }
