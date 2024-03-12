66c1181b32e17c5525a74ea83a10583a7a41e80a
Ivan <opciedjycyt@gmail.com>
2024-03-12 21:52:32 +0800

    feat: add installer testing

---
+++ PractiveProject2/Assets/Scrpits/Installer.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using Zenject;
+
+public class Installer : MonoInstaller
+{
+    public override void InstallBindings()
+    {
+        Container.Bind<A>().AsSingle().NonLazy();
+        Container.Bind(typeof(ITickable)).WithId("B").To<B>().AsSingle();
+        Container.Bind(typeof(ITickable)).To<C>().AsSingle();
+    }
+}
+
+public class A
+{
+    [Inject(Id = "B")]
+    private ITickable _tickableB;
+
+    [Inject]
+    private ITickable _tickableC;
+
+    public A()
+    {
+        Debug.Log("A");
+    }
+}
+
+public class B : ITickable
+{
+    public void Tick()
+    {
+    }
+}
+
+public class C : ITickable
+{
+    public void Tick()
+    {
+    }
+}
